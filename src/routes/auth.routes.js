688c7aba36609aaa046b5a1359fab013aef38f51
Wajahat Ali <wajahatfaryadali@gmail.com>
2026-01-09 05:12:55 +0500

    logout route added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginController, registerController } from "../controllers/auth.controllers.js";
+import { loginController, logOutController, registerController } from "../controllers/auth.controllers.js";
 
 const registerRoute = express.Router();
 
 registerRoute.post("/register", registerController);
 registerRoute.post("/login", loginController);
+registerRoute.post("/logout", logOutController);
 
 export default registerRoute;
