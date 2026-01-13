76575ea45aa4504c30271043c4d45075c556039c
Wajahat Ali <wajahatfaryadali@gmail.com>
2026-01-14 04:43:28 +0500

    next task

--- src/routes/watchlist.routes.js
+++ src/routes/watchlist.routes.js
@@ -13,4 +13,7 @@ watchListRoutes.get("/", getWatchListController);
 
 watchListRoutes.post("/", addToWatchListController);
 
+// now test on post man by addig new watchlist movies and then add delete controller and route by passing id param 
+// also learn about how to handle bulk delete and if body works in delete option
+
 export default watchListRoutes;
